827c073e9f3f5571f731ca4f199ea567867617fd
Amit Saha <justamitsaha@gmail.com>
2026-05-08 21:11:19 +0530

    Update Test.java

--- core-java/src/main/java/testing/Test.java
+++ core-java/src/main/java/testing/Test.java
@@ -8,64 +8,75 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Test {
-        record Employee(
-                        int id,
-                        String name,
-                        String dept,
-                        int age,
-                        double salary) {
-        }
+    record Employee(
+            int id,
+            String name,
+            String dept,
+            int age,
+            double salary) {
+    }
 
-        record Product(
-                        int id,
-                        String name,
-                        String category,
-                        double price) {
-        }
+    record Product(
+            int id,
+            String name,
+            String category,
+            double price) {
+    }
 
-        record Order(
-                        int id,
-                        int customerId,
-                        List<Product> products,
-                        double total) {
-        }
+    record Order(
+            int id,
+            int customerId,
+            List<Product> products,
+            double total) {
+    }
 
-        static List<Employee> employees = Arrays.asList(
-                        new Employee(101, "Amit", "IT", 25, 50000),
-                        new Employee(102, "Rahul", "HR", 30, 65000),
-                        new Employee(103, "Neha", "IT", 27, 55000),
-                        new Employee(104, "Pooja", "FIN", 32, 70000),
-                        new Employee(105, "Karan", "IT", 29, 52000),
-                        new Employee(106, "Simran", "HR", 30, 65000),
-                        new Employee(107, "Vikram", "OPS", 40, 80000),
-                        new Employee(108, "Amit", "IT", 25, 50000), // duplicate data, diff id
-                        new Employee(109, "Riya", "FIN", 28, 70000),
-                        new Employee(110, "Zara", "OPS", 35, 78000));
+    static List<Employee> employees = Arrays.asList(
+            new Employee(101, "Amit", "IT", 25, 50000),
+            new Employee(102, "Rahul", "HR", 30, 65000),
+            new Employee(103, "Neha", "IT", 27, 55000),
+            new Employee(104, "Pooja", "FIN", 32, 70000),
+            new Employee(105, "Karan", "IT", 29, 52000),
+            new Employee(106, "Simran", "HR", 30, 65000),
+            new Employee(107, "Vikram", "OPS", 40, 80000),
+            new Employee(108, "Amit", "IT", 25, 50000), // duplicate data, diff id
+            new Employee(109, "Riya", "FIN", 28, 70000),
+            new Employee(110, "Zara", "OPS", 35, 78000));
 
-        static List<Product> products = Arrays.asList(
-                        new Product(1, "Laptop", "Electronics", 70000),
-                        new Product(2, "Phone", "Electronics", 30000),
-                        new Product(3, "Mouse", "Electronics", 800),
-                        new Product(4, "Shirt", "Clothing", 2000),
-                        new Product(5, "Jeans", "Clothing", 3000),
-                        new Product(6, "Book", "Books", 500),
-                        new Product(7, "Notebook", "Books", 200),
-                        new Product(8, "Laptop", "Electronics", 70000) // duplicate
-        );
+    static List<Product> products = Arrays.asList(
+            new Product(1, "Laptop", "Electronics", 70000),
+            new Product(2, "Phone", "Electronics", 30000),
+            new Product(3, "Mouse", "Electronics", 800),
+            new Product(4, "Shirt", "Clothing", 2000),
+            new Product(5, "Jeans", "Clothing", 3000),
+            new Product(6, "Book", "Books", 500),
+            new Product(7, "Notebook", "Books", 200),
+            new Product(8, "Laptop", "Electronics", 70000) // duplicate
+    );
 
-        static List<Integer> numbers = Arrays.asList(4, 8, 26, 8, 5, 98, 56, 8, 32, 11, 9, 4, 77, 7, 3, 2, 5, 5, 3, 2,
-                        1);
+    static List<Integer> numbers = Arrays.asList(4, 8, 26, 8, 5, 98, 56, 8, 32, 11, 9, 4, 77, 7, 3, 2, 5, 5, 3, 2,
+            1);
 
-        static List<Integer> smallList = new ArrayList<>(List.of(1, 2, 3, 4, 5, 1));
+    static List<Integer> smallList = new ArrayList<>(List.of(1, 2, 3, 4, 5, 1));
 
-        public static void main(String[] args) {
-                frequencyMap(numbers);
-        }
+    public static void main(String[] args) {
+        System.out.println(frequencyMap(numbers));
+        toBinary();
+    }
 
-        private static void frequencyMap(List<Integer> list) {
-                if (null == list || list.isEmpty())
-                        throw new IllegalArgumentException();
-                Map<Integer, Integer> map = new HashMap<>();
-                
-        }
+    private static Map<Integer, Integer> frequencyMap(List<Integer> list) {
+        if (null == list || list.isEmpty())
+            throw new IllegalArgumentException();
+        Map<Integer, Integer> map = new HashMap<>();
+        for (Integer i : list)
+            if (null != map.putIfAbsent(i, 1)) {
+                var count = map.get(i) + 1;
+                map.put(i, count);
+            }
+        return map;
+    }
+
+    private static void toBinary(){
+        String b = "101101";
+        System.out.println(Integer.parseInt(b,2));
+    }
 }
